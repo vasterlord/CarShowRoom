4fd8a012040fcc078e6c82a4df93fa9516520c0c
vasterlord <vasterlord619@gmail.com>
2017-05-02 00:53:40 +0300

    loaded and calculated for  clients

---
+++ CarShowRoom/Model/ClientShowing.cs
@@ -0,0 +1,21 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarShowRoom.Model
+{
+    public class ClientShowing : ObservableObject
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string CarName { get; set; }
+        public string HomeAddress { get; set; }
+        public string PhoneNumber { get; set; }
+        public string DateBuy { get; set; }
+        public double? BuyPrice { get; set; }
+        public double? ServicePercent { get; set; }
+    }
+}
---
+++ CarShowRoom/View/ClientView.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+
+namespace CarShowRoom.View
+{
+    /// <summary>
+    /// Description for ClientView.
+    /// </summary>
+    public partial class ClientView : Window
+    {
+        /// <summary>
+        /// Initializes a new instance of the ClientView class.
+        /// </summary>
+        public ClientView()
+        {
+            InitializeComponent();
+        }
+    }
+}
